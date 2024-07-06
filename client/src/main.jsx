0c174a3b99342aa14b15d1ff7fab98b4c02c1fda
Bianca Harris <bianca.harris@cennox.com>
2024-07-06 19:14:04 +0200

    Companents-and-Pages-added-to-project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
