bc9cb39bd8c769e65522f267088ea46cfa59b7ff
Bianca Harris <bianca.harris@cennox.com>
2024-07-08 12:41:06 +0200

    Added-dummy-data&APIroutes-working-for-questions

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Suggestions from '../components/Suggestions';
 import './Home.css'; 
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Welcome to How-to-life!</h1>
+      <Suggestions/>
 
     </div>
   );
