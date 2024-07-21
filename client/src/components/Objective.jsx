3c09500d32f97a38317230fa5d692ed7a7898c1c
Laura Castro <lauracastrotech@gmail.com>
2024-07-20 22:11:11 -0400

    added loading animation

--- client/src/components/Objective.jsx
+++ client/src/components/Objective.jsx
@@ -3,6 +3,7 @@
 //Objectitve > This section of the form collects more context and has the submit button to send the prompt to the API
 
 import 'animate.css';
+import DotLoader from "react-spinners/DotLoader";
 import React from 'react';
 import { useContext, useState } from 'react';
 import { FormStateContext } from '../helpers/FormContext';
@@ -11,6 +12,7 @@ import axios from 'axios';
 import '../styles/Objective.css'; 
 
 export default function Objective () {
+  const [loading, setLoading] = useState(false);
   const {setFormStatus, setStep, prompt, setPrompt, answer, setAnswer} = useContext(FormStateContext);
   const navigate = useNavigate();
   // Create temporary user id variable to test axios post, once login authentication complete you will need to update
@@ -29,7 +31,7 @@ function handleChange(event) {
   // Send prompt to api
   const handleSubmit = async () => {
     console.log("Submitting prompt:", prompt);
-
+    setLoading(true);
     try {
       console.log("Payload:", { prompt })
       //sends the prompt value to the api
@@ -39,9 +41,9 @@ function handleChange(event) {
 
       console.log("Data response from prompt post method", data);
       setAnswer(data.answer); 
-
       setFormStatus('answer');
       setStep(4);
+      setLoading(false);
       navigate("/answer/4");
     } catch (error) {
       console.log(`That didn't work \n${error}`);
@@ -61,33 +63,37 @@ function handleChange(event) {
 
   return (
     <div className="objective">
-      <div className="container col">
+      {loading && <div className='justify-content-center'>
+                      <DotLoader color='#b7c6e7' size={150}/>
+                    </div>}
+      {!loading && <div className="container col">
         <div className="row animate__animated animate__fadeInLeft">
-            <h2>How do you best learn, why do you need to learn this skill now, what outcome do you hope that you will have, when do you need to do this by.</h2>
-        </div>
-        <div className="row animate__animated animate__fadeInLeft">
-            <form>
-              <div className="form-floating">
-                <textarea
-                className="form-control" 
-                name="prompt" 
-                value={prompt} 
-                onChange={handleChange}
-                id="custom-skill"
-                ></textarea>
-             
-              </div>
-            </form>
-        </div>
-        <div className="progress">
-          <div className="progress-bar" role="progressbar" style={{width: '75%'}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-        <div className="row">
-            <button className='col btn btn-sm' onClick={handleBack}>Back</button>
-            <p className='col d-flex justify-content-center align-items-center' id='step2'>3</p>
-            <button className='col btn btn-sm' onClick={handleSubmit}>Submit</button>
-        </div>
+              <h2>How do you best learn, why do you need to learn this skill now, what outcome do you hope that you will have, when do you need to do this by.</h2>
+          </div>
+          <div className="row animate__animated animate__fadeInLeft">
+              <form>
+                <div className="form-floating">
+                  <textarea
+                  className="form-control" 
+                  name="prompt" 
+                  value={prompt} 
+                  onChange={handleChange}
+                  id="custom-skill"
+                  ></textarea>
+              
+                </div>
+              </form>
+          </div>
+          <div className="progress">
+            <div className="progress-bar" role="progressbar" style={{width: '75%'}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <div className="row">
+              <button className='col btn btn-sm' onClick={handleBack}>Back</button>
+              <p className='col d-flex justify-content-center align-items-center' id='step2'>3</p>
+              <button className='col btn btn-sm' onClick={handleSubmit}>Submit</button>
+          </div> 
       </div>
+      }
     </div>
   );
 }
