fa8756f95ff2858b9cf6a4f36509ce8a0a971194
Sylwia.Orczyk <orczyksylwia@gmail.com>
2024-07-21 21:40:01 +0200

    helper-database

--- client/src/components/Answer.jsx
+++ client/src/components/Answer.jsx
@@ -1,66 +1,3 @@
-
-<<<<<<< HEAD
-||||||| 000ca71
-||||||| ffb4e3b
-=======
-/* eslint-disable no-unused-vars */
->>>>>>> cf16d564cf6e02a52ddd84f674bb62526832ab1f
-=======
-/* eslint-disable no-unused-vars */
->>>>>>> a9f8f532bdc57a26491ce2b84a46054c17aa757b
-// This is the component where chatGpt response renders
-<<<<<<< HEAD
-// GET method for history => this is tied to backend, create list of cards where each card has text that is a question that the user previously asked
-// This is where the text-to-speech icon will live
-
-/*
-step 1 user sees form instructions
-step 2 user selects a category
-step 2a once a category is clicked, add category to prompt, send category name to suggestions api, store popular question in state
-user clicks next
-step 3 render response from suggestions api req, add question/or custom question to prompt state
-user clicks next
-step 4 user types in objective/more context, add to prompt state
-user clicks submit - onClick send prompt to POST api and the reqponse to this request will be rendered in answer component (maybe this call is a useEffect in answer from chatGPT)
-*/
-
-
-||||||| 000ca71
-<<<<<<< HEAD
-// GET method for history => this is tied to backend, create list of cards where each card has text that is a question that the user previously asked
-// This is where the text-to-speech icon will live
-
-/*
-step 1 user sees form instructions
-step 2 user selects a category
-step 2a once a category is clicked, add category to prompt, send category name to suggestions api, store popular question in state
-user clicks next
-step 3 render response from suggestions api req, add question/or custom question to prompt state
-user clicks next
-step 4 user types in objective/more context, add to prompt state
-user clicks submit - onClick send prompt to POST api and the reqponse to this request will be rendered in answer component (maybe this call is a useEffect in answer from chatGPT)
-*/
-
-
-||||||| ffb4e3b
-// GET method for history => this is tied to backend, create list of cards where each card has text that is a question that the user previously asked
-// This is where the text-to-speech icon will live
-
-/*
-step 1 user sees form instructions
-step 2 user selects a category
-step 2a once a category is clicked, add category to prompt, send category name to suggestions api, store popular question in state
-user clicks next
-step 3 render response from suggestions api req, add question/or custom question to prompt state
-user clicks next
-step 4 user types in objective/more context, add to prompt state
-user clicks submit - onClick send prompt to POST api and the reqponse to this request will be rendered in answer component (maybe this call is a useEffect in answer from chatGPT)
-*/
-
-=======
->>>>>>> cf16d564cf6e02a52ddd84f674bb62526832ab1f
-=======
->>>>>>> a9f8f532bdc57a26491ce2b84a46054c17aa757b
 import React from 'react';
 import { useContext, useState, useEffect } from 'react';
 import { FormStateContext } from '../helpers/FormContext';
@@ -102,35 +39,11 @@ export default function Answer() {
         <div className="row m-2">
           <h1 className="title">Ready to learn? Here's your answer</h1>
         </div>
-<<<<<<< HEAD
         <div className="row">
           <h2 className="subtitle">
             You step-by-step guide for mastering [value of category state goes here]
           </h2>
         </div>
-||||||| 000ca71
-||||||| ffb4e3b
-        <div className="row m-2">
-          <h1>Answer</h1>
-        </div>
-        {/* This div will be boiler plate */}
-=======
-        {/* This div will be boiler plate */}
->>>>>>> cf16d564cf6e02a52ddd84f674bb62526832ab1f
-        <div className="row">
-<<<<<<< HEAD
-          <h2 className="subtitle">
-            You step-by-step guide for mastering [value of category state goes here]
-          </h2>
-||||||| ffb4e3b
-            <h2>Here's your How To Life step-by-step guide in the world of [value of category state goes here] </h2>
-=======
-            <h2>[What do we put here]</h2>
->>>>>>> cf16d564cf6e02a52ddd84f674bb62526832ab1f
-        </div>
-=======
-        {/* This div will be boiler plate */}
->>>>>>> a9f8f532bdc57a26491ce2b84a46054c17aa757b
         <div className="row">
           <h2 className="subtitle">Your question: <i>{prompt}</i> </h2>
         </div>
@@ -141,13 +54,6 @@ export default function Answer() {
           <button className="col-4" onClick={handleRegenerate}>
             <img className="icon-group" src="/regenerate.png" alt="regenerate button" />
           </button>
-<<<<<<< HEAD
-||||||| 000ca71
-<<<<<<< HEAD
-=======
-        </div>
-        <div className='row m-2'>
->>>>>>> a9f8f532bdc57a26491ce2b84a46054c17aa757b
           <button className="col-4" onClick={() => handleTextToSpeech(answer)}>
             <img className="icon-group" src="/text_to_speech.png" alt="text to speech button" />
           </button>
--- model/database.js
+++ model/database.js
@@ -1,171 +1,40 @@
-require('dotenv').config();
-const mysql = require('mysql');
+require("dotenv").config();
+const mysql = require("mysql");
+const fs = require("fs");
+// const path = require("path");
 
-const pool = mysql.createPool({
-  connectionLimit: 10, // Adjust this number as per your application's needs
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'how_to_life',
-  port: process.env.DB_PORT || 3306, // Assuming default MySQL port
-  multipleStatements: true
-});
-
-module.exports = function db(query, params) {
-  return new Promise((resolve, reject) => {
-    pool.getConnection((err, connection) => {
-      if (err) {
-        console.error('Error getting MySQL connection:', err);
-        reject(err);
-        return;
-      }
-
-      connection.query(query, params, (error, results) => {
-        connection.release(); // Release the connection back to the pool
-
-        if (error) {
-          console.error('Error executing query:', error.message);
-          reject(error);
-          return;
-        }
-
-        resolve({
-          data: results,
-          affectedRows: results.affectedRows,
-          changedRows: results.changedRows
-        });
-      });
-    });
-  });
-};
-
-//sylwia
-// require("dotenv").config();
-// const mysql = require("mysql");
-// const fs = require("fs");
-// // const path = require("path");
-
-// // Load environment variables
-// const DB_HOST = process.env.DB_HOST;
-// const DB_USER = process.env.DB_USER;
-// const DB_PASS = process.env.DB_PASS;
-// const DB_NAME = process.env.DB_NAME;
-
-// // Create a connection to run the init_db.sql
-// const con = mysql.createConnection(
-//   {
-//       host: DB_HOST || "127.0.0.1",
-//       user: DB_USER || "root",
-//       password: DB_PASS,
-//       database: DB_NAME || "how_to_life",
-//       //port: DB_PORT || "3307",
-//       multipleStatements: true
-//   }
-// );
-
-// con.connect(function(err) {
-
-//     if (err) throw err;
-//     console.log("Connected!");
-  
-//     let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
-//     con.query(sql, function(err, result) {
-//       if (err) throw err;
-//       console.log("Tables creation was successful!");
-  
-//       console.log("Closing...");
-//     });
-  
-//     con.end();
-//   //});
-//   });
-
-
-  //bianca connection
-
-// database.js
-// require("dotenv").config();
-// const mysql = require("mysql");
-// const fs = require("fs");
-// // const path = require("path");
-
-// // Load environment variables
-// const DB_HOST = process.env.DB_HOST;
-// const DB_USER = process.env.DB_USER;
-// const DB_PASS = process.env.DB_PASS;
-// const DB_NAME = process.env.DB_NAME;
-// // const DB_PORT = process.env.DB_PORT;
-
-// // Create a connection to run the init_db.sql
-// const con = mysql.createConnection(
-//   {
-//       host: DB_HOST || "127.0.0.1",
-//       user: DB_USER || "root",
-//       password: DB_PASS,
-//       database: DB_NAME || "how_to_life",
-//       // port: DB_PORT || "3307",
-//       multipleStatements: true
-//   }
-// );
-
-// con.connect(function(err) {
-
-//     if (err) throw err;
-//     console.log("Connected!");
-  
-//     let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
-//     con.query(sql, function(err, result) {
-//       if (err) throw err;
-//       console.log("Tables creation was successful!");
-  
-//       console.log("Closing...");
-//     });
-  
-//     con.end();
-//   });
-
-
-
-  //bianca connection
-
-// database.js
-// require("dotenv").config();
-// const mysql = require("mysql");
-// const fs = require("fs");
-// // const path = require("path");
-
-// // Load environment variables
-// const DB_HOST = process.env.DB_HOST;
-// const DB_USER = process.env.DB_USER;
-// const DB_PASS = process.env.DB_PASS;
-// const DB_NAME = process.env.DB_NAME;
+// Load environment variables
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
 // const DB_PORT = process.env.DB_PORT;
 
-// // Create a connection to run the init_db.sql
-// const con = mysql.createConnection(
-//   {
-//       host: DB_HOST || "127.0.0.1",
-//       user: DB_USER || "root",
-//       password: DB_PASS,
-//       database: DB_NAME || "how_to_life",
-//       port: DB_PORT || "3307",
-//       multipleStatements: true
-//   }
-// );
+// Create a connection to run the init_db.sql
+const con = mysql.createConnection(
+  {
+      host: DB_HOST || "127.0.0.1",
+      user: DB_USER || "root",
+      password: DB_PASS,
+      database: DB_NAME || "how_to_life",
+      // port: DB_PORT || "3307",
+      multipleStatements: true
+  }
+);
 
-// con.connect(function(err) {
+con.connect(function(err) {
 
-//     if (err) throw err;
-//     console.log("Connected!");
+    if (err) throw err;
+    console.log("Connected!");
   
-//     let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
-//     con.query(sql, function(err, result) {
-//       if (err) throw err;
-//       console.log("Tables creation was successful!");
+    let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
+    con.query(sql, function(err, result) {
+      if (err) throw err;
+      console.log("Tables creation was successful!");
   
-//       console.log("Closing...");
-//     });
+      console.log("Closing...");
+    });
   
-//     con.end();
-//   });
+    con.end();
+  });
 
