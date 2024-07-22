5f65c8f9f7df5b1d2bf0d2a03c8739944f91fe9c
Laura Castro <lauracastrotech@gmail.com>
2024-07-21 21:00:06 -0400

    added animation to home view

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,8 @@ import Skill from '../components/Skill';
 import Objective from '../components/Objective';
 import Answer from '../components/Answer';
 import '../styles/Home.css'; 
+import 'animate.css';
+
 
 export default function Home() {
   const { formStatus, setFormStatus, step, setStep } = useContext(FormStateContext);
@@ -35,11 +37,12 @@ export default function Home() {
       {(formStatus === "home" && step === 0) && (
         <div className="home">
           <div className='row'>
-            <div className='col-md-6'>
+            <div className='col-md-6 animate__animated  animate__fadeInLeft'>
               <h1 id='welcome'>Welcome friend,</h1>
-              <p id="description">How to life isyour go-to resource for mastering essential skills you may not have learned in school or just in general. We understand that navigating adulthood can be daunting, whether it&apos;s managing finances, cooking nutritious meals, or understanding complex paperwork. It&apos;s a tool that helps you create a prompt that we'll send to some fancy AI that will give you the best guide. <br/><hr/> Here&apos;s how it works.</p>
+              <p id="description">How to life isyour go-to resource for mastering essential skills you may not have learned in school or just in general. We understand that navigating adulthood can be daunting, whether it&apos;s managing finances, cooking nutritious meals, or understanding complex paperwork. It&apos;s a tool that helps you create a prompt that we'll send to some fancy AI that will give you the best guide. <br/><hr/></p>
+              <h5 className='d-flex justify-content-end'>Here&apos;s how it works.</h5>
             </div>
-            <div className='col-md-6'>
+            <div className='col-md-6 animate__animated  animate__fadeInRight'>
               <div className="card">
                 <p className='card-title'>1 | Category</p>
                 <p className='card-body'>Setup your prompt by selecting an area of interest.</p>
@@ -52,7 +55,6 @@ export default function Home() {
                 <p className='card-title'>3 | Objective</p>
                 <p className='card-body'>Give us some context. The more details the better.</p>
               </div>
-              {/* <hr /> */}
               <button className="btn btn-sm" id='get-started' onClick={handleGetStarted}>Get started</button> 
             </div>
           </div> 
